1504712ea7772b0eb11ab030235b54a608dff14c
tomjin871114 <tom875694231@gmail.com>
2023-02-12 17:52:59 +0800

    Remove unuse

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import gsap from 'gsap';
-import ScrollTrigger from 'gsap/ScrollTrigger';
-import TextPlugin from 'gsap/TextPlugin';
-import MotionPathPlugin from 'gsap/MotionPathPlugin';
 
 import App from './App';
 import './index.css';
-
-gsap.registerPlugin(ScrollTrigger, TextPlugin, MotionPathPlugin);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
